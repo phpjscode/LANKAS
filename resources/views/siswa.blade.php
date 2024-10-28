4e335523d54137f10417ee7ff20ff492d37c47b2
Alan <alanzagi2007@gmail.com>
2024-10-28 19:03:21 +0700

    update

--- resources/views/siswa.blade.php
+++ resources/views/siswa.blade.php
@@ -9,7 +9,7 @@
                             <th class="px-6 py-3">Nama Siswa</th>
                             <th class="px-6 py-3">Jenis Kelamin</th>
                             <th class="px-6 py-3">No. Telepon</th>
-                            <th class="px-6 py-3">Aksi</th>
+                            <th class="px-6 py-3 flex items-center justify-center">Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -18,52 +18,23 @@
                                 <td class="px-6 py-4 font-medium text-gray-900">
                                     {{ $loop->iteration }}
                                 </td>
-                
+
                                 <!-- Nama Siswa -->
-                                <td class="px-6 py-4" x-data="{ editing: false, value: '{{ $item->nama_siswa }}' }">
-                                    <div x-show="!editing" @click="editing = true" class="cursor-pointer">
-                                        <span x-text="value"></span>
-                                    </div>
-                                    <input 
-                                        x-show="editing" 
-                                        type="text" 
-                                        x-model="value" 
-                                        @keydown.enter="update('{{ route('siswa.update', $item->id) }}', { nama_siswa: value })" 
-                                        @click.away="editing = false" 
-                                        class="border px-2 py-1 w-full">
-                                </td>
-                
-                                <!-- Jenis Kelamin -->
-                                <td class="px-6 py-4" x-data="{ editing: false, value: '{{ $item->jenis_kelamin }}' }">
-                                    <div x-show="!editing" @click="editing = true" class="cursor-pointer">
-                                        <span x-text="value"></span>
-                                    </div>
-                                    <select 
-                                        x-show="editing" 
-                                        x-model="value" 
-                                        @change="update('{{ route('siswa.update', $item->id) }}', { jenis_kelamin: value })"
-                                        @click.away="editing = false"
-                                        class="border px-2 py-1 w-full">
-                                        <option value="Laki-laki">Laki-laki</option>
-                                        <option value="Perempuan">Perempuan</option>
-                                    </select>
-                                </td>
-                
-                                <!-- No. Telepon -->
-                                <td class="px-6 py-4" x-data="{ editing: false, value: '{{ $item->no_telepon }}' }">
-                                    <div x-show="!editing" @click="editing = true" class="cursor-pointer">
-                                        <span x-text="value"></span>
-                                    </div>
-                                    <input 
-                                        x-show="editing" 
-                                        type="text" 
-                                        x-model="value" 
-                                        @keydown.enter="update('{{ route('siswa.update', $item->id) }}', { no_telepon: value })" 
-                                        @click.away="editing = false" 
-                                        class="border px-2 py-1 w-full">
-                                </td>
-                
                                 <td class="px-6 py-4">
+                                    {{ $item->nama_siswa }}
+                                </td>
+
+                                <!-- Jenis Kelamin -->
+                                <td class="px-6 py-4">
+                                    {{ $item->jenis_kelamin }}
+                                </td>
+
+                                <!-- No. Telepon -->
+                                <td class="px-6 py-4">
+                                    {{ $item->no_telepon }}
+                                </td>
+
+                                <td class="px-6 py-4 flex items-center justify-center">
                                     <button class="text-red-500 hover:underline">Hapus</button>
                                 </td>
                             </tr>
@@ -74,32 +45,6 @@
                         @endforelse
                     </tbody>
                 </table>
-                
-                <script>
-                    function update(url, data) {
-                        fetch(url, {
-                            method: 'PATCH',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                            },
-                            body: JSON.stringify(data)
-                        })
-                        .then(response => {
-                            if (!response.ok) {
-                                throw new Error('Gagal memperbarui data!');
-                            }
-                            return response.json();
-                        })
-                        .then(result => {
-                            alert('Data berhasil diperbarui!');
-                        })
-                        .catch(error => {
-                            console.error('Error:', error);
-                            alert('Terjadi kesalahan saat memperbarui data.');
-                        });
-                    }
-                </script>                
             </div>
         </main>
     @endsection
