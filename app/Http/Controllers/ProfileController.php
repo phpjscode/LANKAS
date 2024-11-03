e60227e5434d4322d1ba00a74ca89ec42becb13e
Alan <alanzagi2007@gmail.com>
2024-11-03 19:17:44 +0700

    update profil

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 
-class ProfileController extends Controller
+class ProfileController extends Controlle
 {
     public function showProfile()
     {
