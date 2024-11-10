3cf81c4866823e1e43664abeb945a728edf8717e
Alan <alanzagi2007@gmail.com>
2024-11-10 13:35:01 +0700

    stable keknya

--- resources/views/components/table-siswa.blade.php
+++ resources/views/components/table-siswa.blade.php
@@ -35,6 +35,112 @@
     </div>
 </div>
 
+
+<!-- Modal Ubah Siswa -->
+<div id="editModal" class="editModal hidden fixed inset-0 bg-gray-900 bg-opacity-50 items-center justify-center p-4">
+    <div class="bg-white p-6 rounded-lg shadow-lg w-full sm:w-1/3">
+        <div class="flex items-center justify-between">
+            <h2 class="text-xl mb-4">Edit Siswa</h2>
+            <button type="button"
+                class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center close-modal-btn">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+        </div>
+        <form id="editForm">
+            @csrf
+            @method('PATCH')
+            <input type="hidden" id="siswaId" name="id">
+
+            <div class="mb-4">
+                <label for="nama_siswa" class="block text-sm font-medium text-gray-700">Nama Siswa</label>
+                <input type="text" id="nama_siswa" name="nama_siswa" class="mt-1 p-2 border rounded w-full" required>
+            </div>
+
+            <div class="mb-4">
+                <label class="block text-sm font-medium text-gray-700">Jenis Kelamin</label>
+                <div class="flex gap-4">
+                    <label>
+                        <input type="radio" name="jenis_kelamin" value="Laki-laki" class="jkLaki" required>
+                        Laki-laki
+                    </label>
+                    <label>
+                        <input type="radio" name="jenis_kelamin" value="Perempuan" class="jkPerempuan">
+                        Perempuan
+                    </label>
+                </div>
+            </div>
+
+            <div class="mb-4">
+                <label for="no_telepon" class="block text-sm font-medium text-gray-700">No. Telepon</label>
+                <input type="text" id="no_telepon" name="no_telepon" class="mt-1 p-2 border rounded w-full" required>
+            </div>
+
+            <div class="flex justify-end">
+                <button type="button"
+                    class="mr-2 bg-gray-500 text-white px-4 py-2 rounded close-modal-btn">Batal</button>
+                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Simpan</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+<!-- Modal Form Tambah Siswa -->
+<div id="tambahModal" class="hidden fixed inset-0 bg-gray-900 bg-opacity-50 items-center justify-center p-4">
+    <div class="bg-white p-6 rounded-lg shadow-lg w-full max-w-md sm:w-1/3">
+        <div class="flex items-center justify-between pb-5">
+            <h3 class="text-xl text-gray-900">Tambah Siswa</h3>
+            <button type="button"
+                class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center close-modal-btn">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+        </div>
+        <form id="tambahForm">
+            @csrf
+            <div class="mb-4">
+                <label for="nama_siswa" class="block text-sm font-medium text-gray-700">Nama Siswa</label>
+                <input type="text" id="nama_siswa" name="nama_siswa" class="mt-1 p-2 border rounded w-full" required>
+            </div>
+
+            <div class="mb-4">
+                <label class="block text-sm font-medium text-gray-700">Jenis Kelamin</label>
+                <div class="flex gap-4">
+                    <label>
+                        <input type="radio" name="jenis_kelamin" value="Laki-laki" required>
+                        Laki-laki
+                    </label>
+                    <label>
+                        <input type="radio" name="jenis_kelamin" value="Perempuan" required>
+                        Perempuan
+                    </label>
+                </div>
+            </div>
+
+            <div class="mb-4">
+                <label for="no_telepon" class="block text-sm font-medium text-gray-700">No. Telepon</label>
+                <input type="tel" id="no_telepon" name="no_telepon" class="mt-1 p-2 border rounded w-full"
+                    required>
+            </div>
+
+            <div class="flex justify-end">
+                <button type="button"
+                    class="mr-2 bg-gray-500 text-white px-4 py-2 rounded close-modal-btn">Batal</button>
+                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Simpan</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+
 <script>
     $(document).ready(function() {
         // Setup CSRF Token agar setiap request AJAX aman
